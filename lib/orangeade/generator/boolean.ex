dbc5a67b8d88237d18f41e3570cb545c99ddb3a4
kkirahvi <pof@student.agh.edu.pl>
2018-04-12 13:39:13 +0200

    doc for boolean

--- lib/orangeade/generator/boolean.ex
+++ lib/orangeade/generator/boolean.ex
@@ -1,7 +1,21 @@
 defmodule Orangeade.Generator.Boolean do
-  alias Orangeade.Generator.BoundNatural, as: BoundNatural
+  @moduledoc """
+  A boolean values generator
   
+  
+  ## Examples
+
+      iex(1)> Caffeine.Stream.take(
+      ...(1)> Orangeade.Generator.Boolean.stream(),
+      ...(1)> 5_000)
+      [false, false, nil, nil, nil, nil, false, true, false, nil, nil, nil, nil, true,
+      true, nil, nil, nil, nil, false, nil, false, nil, false, nil, false, true,
+      false, true, false, true, false, nil, false, true, false, false, false, false,
+      false, false, true, nil, false, false, false, false, nil, true, true, ...]
+
+  """
   def stream do
+    alias Orangeade.Generator.BoundNatural, as: BoundNatural
     BoundNatural.stream(limit: 3)
       |> Caffeine.Stream.map(&bool/1)
     end

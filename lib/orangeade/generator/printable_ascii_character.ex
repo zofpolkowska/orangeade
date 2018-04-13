88f8568f4db06e8d476ac1f2ccc4d9b96cf50289
seb <sebastian.bucki@gmail.com>
2018-04-13 02:37:02 +0200

    Add module for generating characters and tests for it

---
+++ lib/orangeade/generator/printable_ascii_character.ex
@@ -0,0 +1,42 @@
+defmodule Orangeade.Generator.PrintableASCIICharacter do
+  @moduledoc """
+  Provides a function for creating a stream of ASCII characters.
+  """
+
+  alias Orangeade.Generator.BoundNatural
+
+  @doc """
+  Creates a stream of ASCII characters.
+  
+  ## Examples
+
+      iex(1)> s = Orangeade.Generator.PrintableASCIICharacter.stream()
+      [33 | #Function<1.23635164/0 in Caffeine.Stream.map/2>]
+
+      iex(2)> Caffeine.Stream.take(s, 10)
+      '!he@E@?l{$'
+
+  """
+  @spec stream() :: list
+  def stream do
+    Caffeine.Stream.map(BoundNatural.stream(limit: length()),
+      &to_number_between_bounds/1)
+  end
+
+  defp to_number_between_bounds(n) do
+    lower_bound() + n 
+  end
+
+  defp length do
+    upper_bound() - lower_bound()
+  end
+
+  defp lower_bound do
+    0x20
+  end
+
+  defp upper_bound do
+    0x7E
+  end
+end
+
--- test/orangeade_test.exs
+++ test/orangeade_test.exs
@@ -12,5 +12,8 @@ defmodule OrangeadeTest do
     assert Orangeade.Generator.BoundNatural.__info__(:functions) == [
              stream: 1
            ]
+    assert Orangeade.Generator.PrintableASCIICharacter.__info__(:functions) == [
+             stream: 0
+           ]
   end
 end

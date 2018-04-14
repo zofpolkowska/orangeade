a602558fc760ddc8a196c76258daf5fbbc9c5eca
kkirahvi <pof@student.agh.edu.pl>
2018-04-14 21:37:20 +0200

    adding ascii test

--- lib/orangeade/generator/printable_ascii.ex
+++ lib/orangeade/generator/printable_ascii.ex
@@ -1,14 +1,35 @@
 defmodule Orangeade.Generator.Printable.ASCII do
-  alias Orangeade.Generator.BigNatural
-  defmodule V1 do
-    def stream do
-      Caffeine.Stream.map(BigNatural.instance(),
-	fn e -> Integer.mod(e, printable()) end)
-    end
+  @moduledoc """
+  Provides a funcion to generate printable ascii characters
+  """
 
-    def printable do
-      0x7E - 0x20
-    end
+  @doc """
+  Generates a stream of printable ascii characters
+
+  # Example
+  iex> Caffeine.Stream.take(                        
+  ...> Orangeade.Generator.Printable.ASCII.stream(),
+  ...> 100)                                         
+  '!he@E@?l{$w@7$O:!2[:-LIBG@[&!^7:gp]f% W4S8qjgve,s@#$}vul?B;(GLGZ_J9Ha>!B%8#T]6q^iD[hY"kJM\\-8u4?,-J_:'
+
+  """
+  def stream do
+    alias Psi.LinearCongruentialGenerator, as: LCG
+    
+    s = LCG.stream(generator: Orangeade.Generator.BigNatural.instance(), seed: 1)
+    Caffeine.Stream.map(s, fn e -> Integer.mod(e, printable()) + lower() end )
   end
-
+  
+  defp printable do
+    upper() - lower()
+  end
+  
+  defp lower do
+    0x20
+  end
+  
+  defp upper do
+    0x7E
+  end
+  
 end
---
+++ test/printable_ascii_test.exs
@@ -0,0 +1,9 @@
+defmodule PrintableASCIITest do
+  use ExUnit.Case
+  test "only printable ascii characters are generated" do
+    s = Caffeine.Stream.take(                    
+      Orangeade.Generator.Printable.ASCII.stream(),
+      1_000)
+    assert List.ascii_printable?(s)
+  end
+end

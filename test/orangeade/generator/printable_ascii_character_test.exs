076f5be1301a1a4001410fcbfe23234d2c519181
seb <sebastian.bucki@gmail.com>
2018-04-15 20:13:19 +0200

    Reomve unnecessary test

--- test/orangeade/generator/printable_ascii_character_test.exs
+++ test/orangeade/generator/printable_ascii_character_test.exs
@@ -8,17 +8,6 @@ defmodule PrintableASCIICharacterTest do
     assert Enum.all?(charlist, &is_in_range/1)
   end
 
-  test "First 20 ascii numbers are non-negative" do
-    charlist = PrintableASCIICharacter.stream()
-    |> Caffeine.Stream.take(20)
-
-    assert Enum.all?(charlist, &non_negative/1)
-  end
-
-
   defp is_in_range(n) when n >= 32 and n <= 126, do: true
   defp is_in_range(_n), do: false
-
-  defp non_negative(n) when n >= 0, do: true
-  defp non_negative(_n), do: false
 end

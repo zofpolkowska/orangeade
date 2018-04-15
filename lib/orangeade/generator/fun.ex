d40ab6ecaabdd2e5d2b9d84c50626a1ef36f25ad
kkirahvi <pof@student.agh.edu.pl>
2018-04-15 14:12:27 +0200

    adding function generator with tests

--- lib/orangeade/generator/fun.ex
+++ lib/orangeade/generator/fun.ex
@@ -1,41 +1,83 @@
 defmodule Orangeade.Generator.Fun do
-  @defmodule """
+  @moduledoc """
   Provides functions to generate streams of functions
   """
 
-  @def """
+  @doc """
   Generates a stream of functions from the Kernel module
 
   # Example
-  iex> Caffeine.Stream.take(
-  ...> Orangeade.Generator.Fun.stream(),
-  ...> 5)
-  [#Function<0.126840938/1 in Orangeade.Generator.Fun.f/1>,
-  #Function<0.126840938/1 in Orangeade.Generator.Fun.f/1>,
-  #Function<0.126840938/1 in Orangeade.Generator.Fun.f/1>,
-  #Function<0.126840938/1 in Orangeade.Generator.Fun.f/1>,
-  #Function<0.126840938/1 in Orangeade.Generator.Fun.f/1>]
+  iex> [fun | rest] = Caffeine.Stream.take(
+  ...> Orangeade.Generator.Fun.stream(),   
+  ...> 8)                                  
+  [#Function<2.33900811/2 in Orangeade.Generator.Fun.f/1>,
+  #Function<2.33900811/2 in Orangeade.Generator.Fun.f/1>,
+  #Function<1.33900811/1 in Orangeade.Generator.Fun.f/1>,
+  #Function<1.33900811/1 in Orangeade.Generator.Fun.f/1>,
+  #Function<1.33900811/1 in Orangeade.Generator.Fun.f/1>,
+  #Function<3.33900811/3 in Orangeade.Generator.Fun.f/1>,
+  #Function<1.33900811/1 in Orangeade.Generator.Fun.f/1>,
+  #Function<2.33900811/2 in Orangeade.Generator.Fun.f/1>]
+  iex(19)> i(fun)
+  Term
+    #Function<2.33900811/2 in Orangeade.Generator.Fun.f/1>
+  Data type
+    Function
+  Type
+    local
+  Arity
+    2
+  Description
+    This is an anonymous function.
+  Implemented protocols
+    Enumerable, IEx.Info, Inspect
+
   """
   def stream do
     range = Enum.count(Kernel.__info__(:functions))
     Caffeine.Stream.map(Orangeade.Generator.BoundNatural.stream(limit: range),
-      &f/1)
+      fn e -> f(Enum.at(Kernel.__info__(:functions), e)) end)
   end
 
+  @doc"""
+  Generates a stream of Kernel functions with identifiers
+
+  # Example
+
+  iex> Caffeine.Stream.take(                 
+  ...> Orangeade.Generator.Fun.stream_named(),
+  ...> 4)
+  [
+    {:!==, 2, #Function<2.33900811/2 in Orangeade.Generator.Fun.f/1>},
+    {:>=, 2, #Function<2.33900811/2 in Orangeade.Generator.Fun.f/1>},
+    {:spawn_link, 1, #Function<1.33900811/1 in Orangeade.Generator.Fun.f/1>},
+    {:is_port, 1, #Function<1.33900811/1 in Orangeade.Generator.Fun.f/1>}
+  ]
+  """
   def stream_named do
     range = Enum.count(Kernel.__info__(:functions))
     Caffeine.Stream.map(Orangeade.Generator.BoundNatural.stream(limit: range),
-      &fnamed/1)
-  end
-  
-  defp f(p) do
-    {name, _} = Enum.at(Kernel.__info__(:functions), p)
-    fn args -> apply(Kernel, name, args) end
+      fn e -> fnamed(Enum.at(Kernel.__info__(:functions), e)) end)
   end
 
-  defp fnamed(p) do
-    {name, arity} = Enum.at(Kernel.__info__(:functions), p)
-    {name, arity, fn args -> apply(Kernel, name, args) end}
+  defp f({name, arity}) do
+    cond do
+      arity === 0 ->
+	fn -> apply(Kernel,name, []) end
+      arity === 1 ->
+	fn e -> apply(Kernel, name, [e]) end
+      arity === 2 ->
+	fn d, e -> apply(Kernel, name, [d, e]) end
+      arity === 3 ->
+	fn c, d, e -> apply(Kernel, name, [c, d, e]) end
+      true ->
+	fn -> "default" end
+    end
   end
-  
-end
+
+  defp fnamed({name, arity}) do
+    {name, arity, f({name, arity})}
+  end
+
+end 
+

61b49e9d11eda3ef8edea31f342f3decf4077bfa
seb <sebastian.bucki@gmail.com>
2018-04-16 00:11:32 +0200

    Add logical change in test

--- test/orangeade/generator/term_test.exs
+++ test/orangeade/generator/term_test.exs
@@ -12,7 +12,8 @@ defmodule Orangeade.Generator.TermTest do
       Enum.any?(list_of_elements, &is_binary/1),
       Enum.any?(list_of_elements, &is_integer/1),
       Enum.any?(list_of_elements, &is_float/1),
-      Enum.any?(list_of_elements, &boolean_or_nil/1)
+      Enum.any?(list_of_elements, &is_boolean/1),
+      Enum.any?(list_of_elements, &is_nil/1)
     ]
     assert Enum.all?(list_of_conditions)
   end
@@ -22,9 +23,4 @@ defmodule Orangeade.Generator.TermTest do
 
   defp is_in_range_32_126(n) when n >= 32 and n <= 126, do: true
   defp is_in_range_32_126(_n), do: false
-
-  defp boolean_or_nil(n) when is_boolean(n), do: true
-  defp boolean_or_nil(n) when n == nil, do: true
-  defp boolean_or_nil(_n), do: false
-
 end

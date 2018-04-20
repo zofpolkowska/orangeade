097200e5e17088f3b515a9811f8abfd46ce5d71d
seb <sebastian.bucki@gmail.com>
2018-04-20 20:25:11 +0200

    Add additional logical changes
    No need for second test in PrintableASCIICharacterTest module comparing to master.

--- test/orangeade/generator/printable_ascii_character_test.exs
+++ test/orangeade/generator/printable_ascii_character_test.exs
@@ -2,12 +2,9 @@ defmodule PrintableASCIICharacterTest do
   use ExUnit.Case
   alias Orangeade.Generator.PrintableASCIICharacter
 
-  test "First 20 ascii numbers are in the range <32, 126>" do
-    charlist = PrintableASCIICharacter.stream()
+  test "First 20 elements are printable ascii characters" do
+    list = PrintableASCIICharacter.stream()
     |> Caffeine.Stream.take(20)
-    assert Enum.all?(charlist, &is_in_range/1)
+    assert List.ascii_printable?(list)
   end
-
-  defp is_in_range(n) when n >= 32 and n <= 126, do: true
-  defp is_in_range(_n), do: false
 end
